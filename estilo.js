2099654a1f69f61462b806a08a43173f0d67d9de
Rokobungi <58945440+Rokobungi@users.noreply.github.com>
2020-06-10 17:35:52 -0400

    Update estilo.js

--- estilo.js
+++ estilo.js
@@ -4,3 +4,24 @@ function llamarFuente(fuente){
   enlace.setAttribute("href",fuente);
   document.head.appendChild(enlace);
 }
+
+function definirEstilos(fuente){
+  var estilo = document.createElement("STYLE");
+  var texto = document.createTextNode("\
+		@font-face {\
+			font-family: "+fuente+"\
+    }\
+    .titulo{\
+      font-size: 32px;\
+			font-family: "+fuente+"\
+			margin-left: 20px;\
+    }\
+    .descripcion{\
+      ont-size: 20px;\
+			font-family: "+fuente+"\
+			margin-left: 10px;\
+    }\
+  ");
+  estilo.appendChild(texto);
+  document.head.appendChiled(estilo);
+}
