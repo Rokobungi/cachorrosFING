f504ae4113b8be3d0224becc0a9a4e128b64ff43
Rokobungi <58945440+Rokobungi@users.noreply.github.com>
2020-06-10 20:21:54 -0400

    Update estilo.js

--- estilo.js
+++ estilo.js
@@ -3,14 +3,14 @@ function definirEstilos(){
 	var texto = document.createTextNode("\
 		@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;900&display=swap');\
 		#pTitulo{\
-			font-size: 25px;\
+			font-size: 24px;\
 			font-family: 'Montserrat', sans-serif;\
-			margin-left: 20px;\
+			margin-left: 18px;\
 			position: absolute;\
-			top: 90%;\
+			top: 80%;\
 		}\
 		#pDescripcion{\
-			ont-size: 18px;\
+			ont-size: 14px;\
 			font-family: 'Montserrat', sans-serif;\
 			margin-left: 10px;\
 		}\
