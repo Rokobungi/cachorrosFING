ceb8efcd325bab486c96703082681fd0bbf41261
Rokobungi <58945440+Rokobungi@users.noreply.github.com>
2020-06-10 20:28:42 -0400

    Update estilo.js

--- estilo.js
+++ estilo.js
@@ -5,8 +5,7 @@ function definirEstilos(){
 		#pTitulo{\
 			font-size: 24px;\
 			font-family: 'Montserrat', sans-serif;\
-			margin-left: 18px;\
-			top: 80%;\
+			top: 70%;\
 		}\
 		#pDescripcion{\
 			ont-size: 14px;\
